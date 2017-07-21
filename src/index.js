a46a068042ca153ed5b5266eaab258091ce82bff
Raymond Julin <raymond.julin@gmail.com>
2017-07-21 15:17:49 +0200

    Export Grid and ProgressBar for build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import * as styles from './styles'
 import Button from './components/Button'
+import ProgressBar from './components/ProgressBar'
+import Grid from './components/Grid'
 
 export {
+  Grid,
   Button,
+  ProgressBar,
   styles,
 }
