f1b96d7ba4779a10bc8873c08dde9734d13fbe77
Raymond Julin <raymond.julin@gmail.com>
2017-07-26 13:21:55 +0200

    Add Number, Percentage and PieChart to dist build

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import * as styles from './styles'
 import Button from './components/Button'
 import ProgressBar from './components/ProgressBar'
 import Grid from './components/Grid'
+import Number from './components/Number'
+import Percentage from './components/Percentage'
+import PieChart from './components/PieChart'
 
 export {
   Grid,
   Button,
   ProgressBar,
+  Number,
+  Percentage,
+  PieChart,
   styles,
 }
