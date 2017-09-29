c5fff8b193c5a48e6fd3921992e928aa7d9d5015
Raymond Julin <raymond.julin@gmail.com>
2017-09-29 11:18:03 +0200

    Add spacing.xsmall for ready-made unit spacing

--- src/styles/spacing.js
+++ src/styles/spacing.js
@@ -1,6 +1,7 @@
 export const unit = 6
 const multiply = (factor = 1) => `${factor * unit}px`
 
+export const xsmall = multiply(1)
 export const small = multiply(2)
 export const base = multiply(4)
 export const large = multiply(10)
