869540b6c8b05fc2480c04af0a69219df79937e1
Simon Bognø <simonbognoe@gmail.com>
2018-01-04 16:21:42 +0100

    Add text color

--- src/components/CompletionCard/styled.js
+++ src/components/CompletionCard/styled.js
@@ -2,6 +2,7 @@ import glamorous from 'glamorous'
 import theme from '../../styles/theme'
 import {completionGradient} from '../../styles/mixins'
 import {fontSize} from '../../styles/mixins'
+import {colors} from '../../styles/colors'
 import {Box} from '../Grid'
 import Heading from '../Heading'
 import Text from '../Text'
@@ -13,9 +14,10 @@ export const StyledBox = glamorous(Box)({
   transition: 'box-shadow .2s',
   display: 'inline-block',
   width: '100%',
-  // '&:hover': {
-  //   boxShadow: '0 2px 6px 0 rgba(0,0,0,.2)',
-  // }
+  color: colors.text,
+  '&:hover': {
+    boxShadow: '0 2px 6px 0 rgba(0,0,0,.1)',
+  }
 })
 
 export const StyledHeading = glamorous(Heading)({
