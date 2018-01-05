405a756e3be965c69a98c18d4da3d492c30f0369
Dilan Srilal <dilan@taskanalytics.com>
2018-01-05 11:21:12 +0530

    Color for Logo text

--- src/components/Logo/styled.js
+++ src/components/Logo/styled.js
@@ -1,6 +1,8 @@
 import glamorous from 'glamorous'
 import Text from '../Text'
+import {colors} from '../../styles/colors'
 
 export const StyledLogo = glamorous(Text)({
   fontWeight: 'bold',
+  color: colors.text,
 })
