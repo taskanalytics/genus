f8d280be9bd0400538db9eca2d51ca4dc942b1c3
Dilan Srilal <dilan@taskanalytics.com>
2018-01-05 15:08:41 +0530

    EntityCard color

--- src/components/EntityCard/styled.js
+++ src/components/EntityCard/styled.js
@@ -3,12 +3,13 @@ import theme from '../../styles/theme'
 import {fontSize} from '../../styles/mixins'
 import Heading from '../Heading'
 import {Flex} from '../Grid'
-
+import {colors} from '../../styles/colors'
 export const StyledFlex = glamorous(Flex)({
   alignItems: 'center',
   backgroundColor: theme.colors.white,
   borderRadius: theme.radius*2,
   transition: 'box-shadow .2s',
+  color: colors.text,
   '&:hover': {
     boxShadow: '0 2px 6px 0 rgba(0,0,0,.2)',
   }
