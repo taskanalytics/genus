fb500887b9d8751122103573eff545c4ce832345
Raymond Julin <raymond.julin@gmail.com>
2018-03-15 12:31:37 +0100

    Add support for <Heading align=value>

--- src/components/Heading/styled.js
+++ src/components/Heading/styled.js
@@ -2,6 +2,7 @@ import glamorous from 'glamorous'
 import withIsProp from '../../utils/withIsProp'
 import width from 'styled-system/dist/width'
 import space from 'styled-system/dist/space'
+import textAlign from 'styled-system/dist/text-align'
 
 export const StyledHeading = withIsProp(glamorous('h1')(
   [{
@@ -9,4 +10,5 @@ export const StyledHeading = withIsProp(glamorous('h1')(
   }],
   width,
   space,
+  textAlign,
 ))
