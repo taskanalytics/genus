f90482a14ec790351e9d981bc921296efc6f1842
Raymond Julin <raymond.julin@gmail.com>
2018-04-16 14:51:31 +0200

    Better error handling for withReduxForm

--- src/components/Form/withReduxForm.js
+++ src/components/Form/withReduxForm.js
@@ -3,10 +3,11 @@ import React from 'react'
 // As we have code using redux-form we need to supply a HOC to wrap
 export default function withReduxForm (Wrapped) {
   return ({ input, meta, ...rest }) => {
+    const { dirty, error, submitFailed } = meta
     return <Wrapped
       {...rest}
       {...input}
-      error={meta.error}
+      error={ error && (dirty || submitFailed) ? error : null }
     />
   }
 }
