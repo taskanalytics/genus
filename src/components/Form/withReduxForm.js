83b3ad0dcbfd423cfc7c430a9c2e7fbf280cd130
Raymond Julin <raymond.julin@gmail.com>
2018-04-16 14:05:31 +0200

    Fix missing import of React in Form.withReduxForm

--- src/components/Form/withReduxForm.js
+++ src/components/Form/withReduxForm.js
@@ -1,3 +1,5 @@
+import React from 'react'
+
 // As we have code using redux-form we need to supply a HOC to wrap
 export default function withReduxForm (Wrapped) {
   return ({ input, meta, ...rest }) => {
