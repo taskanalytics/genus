7294b4bef361f062f859711e3cb7d77678404f2c
Raymond Julin <raymond.julin@gmail.com>
2018-06-29 01:23:06 +0200

    Improve rendering performance of <EntityCard> with a better SCU

--- src/components/EntityCard/index.js
+++ src/components/EntityCard/index.js
@@ -1,10 +1,20 @@
-import React, { PureComponent } from 'react'
+import React, { Component } from 'react'
 import T from 'prop-types'
+import isEqual from 'lodash/isEqual'
 import { StyledCard, StyledBox, StyledHeading } from './styled'
 import Text from '../Text'
 import Avatar from '../Avatar'
 
-class EntityCard extends PureComponent {
+class EntityCard extends Component {
+  shouldComponentUpdate (nextProps) {
+    const simple = ['name', 'description'].some(prop => {
+      return nextProps[prop] !== this.props[prop]
+    })
+    return simple || nextProps.actions.some((action, index) => {
+      return action.name !== this.props.actions[index].name
+    })
+  }
+
   render () {
     const {name, description, avatar, ...props} = this.props
     return (
@@ -24,6 +34,7 @@ if (process.env.NODE_ENV !== 'production') {
     name: T.string.isRequired,
     description: T.string,
     avatar: T.string,
+    actions: T.array,
   }
 }
 
