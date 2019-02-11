d6b5b75e85a69b16df8b04dfc03c8ea8ffadb6d2
Raymond Julin <raymond.julin@gmail.com>
2019-02-11 10:32:33 +0100

    Replace ava with jestt+react-testing-library

--- src/components/Number/test.js
+++ src/components/Number/test.js
@@ -1,27 +1,25 @@
 import React from 'react'
-import test from 'ava'
-import { renderJSX, JSX } from 'jsx-test-helpers'
+import { render } from 'react-testing-library'
 
-import Number from './index.js'
+import NumberComponent from './index.js'
 
-test(`<Number> renders a proper <dl> structure`, t => {
-  const actual = renderJSX(
-    <Number description="Test" value={10} />
-  )
-  const expected = JSX(
-    <dl className="number">
-      <dt className="number__value">10</dt>
-      <dd className="number__description">Test</dd>
-    </dl>
-  )
+describe('<Number> component', () => {
+  it('renders with just value as span', () => {
+    const { container } = render(<NumberComponent value={10} />)
+    const node = container.firstChild
+    expect(node).toMatchSnapshot()
+    expect(node.tagName).toBe('SPAN')
+    expect(node.textContent).toBe('10')
+  })
 
-  t.is(actual, expected)
-})
-
-test(`<Number> supports --small`, t => {
-  const actual = renderJSX(
-    <Number description="Test" value={10} size="small" />
-  )
-
-  t.true(actual.includes('className="number number--small"'))
+  it('renders with description as dl', () => {
+    const { container } = render(<NumberComponent description="Test" value={10} />)
+    const node = container.firstChild
+    expect(node).toMatchSnapshot()
+    expect(node.tagName).toBe('DL')
+    expect(node.firstChild.tagName).toBe('DT')
+    expect(node.firstChild.textContent).toBe('10')
+    expect(node.lastChild.tagName).toBe('DD')
+    expect(node.lastChild.textContent).toBe('Test')
+  })
 })
