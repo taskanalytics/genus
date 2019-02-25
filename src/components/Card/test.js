b1f951ae1d4205c23cba6b96dbf83c33c4daf3f1
Raymond Julin <raymond.julin@gmail.com>
2019-02-25 09:08:08 +0100

    Test if clicking card actions actually renders them

--- src/components/Card/test.js
+++ src/components/Card/test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {ThemeProvider} from 'emotion-theming'
-import { render } from 'react-testing-library'
+import { render, fireEvent, waitForElement } from 'react-testing-library'
 
 import theme from '../../styles/theme'
 import Card from './index.js'
@@ -20,7 +20,7 @@ describe('<Card> component', () => {
     expect(node.textContent).toBe('Text')
   })
 
-  it('renders actions', () => {
+  it('renders actions when clicked', async () => {
     const { container } = renderComponent({
       children: <span>Text</span>,
       actions: [{name: 'Test'}],
@@ -28,7 +28,16 @@ describe('<Card> component', () => {
     const node = container.firstChild
     expect(node).toMatchSnapshot()
     expect(node.tagName).toBe('DIV')
+    const actionsContainer = node.querySelector('[data-genus="Dropdown"]')
     const button = node.querySelector('div button')
     expect(button.firstChild.tagName).toBe('svg')
+
+    fireEvent.click(button)
+
+    const action = await waitForElement(() =>
+      actionsContainer.querySelector('div[open]>div')
+    )
+
+    expect(action.textContent).toBe('Test')
   })
 })
